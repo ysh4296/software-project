4457325bb1f1d070f531eb416f9228e8a7f11ecb
heoboseong7 <boseong844@gmail.com>
2021-05-30 17:21:27 +0900

    Add logout url

--- develop/users/urls.py
+++ develop/users/urls.py
@@ -4,8 +4,9 @@ from . import views
 app_name = "users"
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
-    path('login/', views.LoginView.as_view(), name='login'),
-    path('signup/', views.SignUpView.as_view(), name='signup'),
-    path('edit/', views.EditView.as_view(), name='edit')
+    path("home/", views.home, name="home"),
+    path("login/", views.LoginView.as_view(), name="login"),
+    path("signup/", views.SignUpView.as_view(), name="signup"),
+    path("edit/", views.EditView.as_view(), name="edit"),
+    path(("logout/"), views.log_out, name="logout"),
 ]
